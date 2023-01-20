0f9f1a7eb29f7a60dc304bb7cd8a9177079ad9d0
williamcsa04 <williamcaetano14@gmail.com>
2023-01-20 00:45:01 -0300

    :sparkles: Implemented logic to load data to champion specific page

--- src/services/riot.ts
+++ src/services/riot.ts
@@ -1,13 +1,61 @@
 import { BASE_DRAGON_URL } from "../config/riot";
 
-export interface Data {
+export interface Data<T> {
     type:    Type;
     format:  string;
     version: Version;
-    data:    { [key: string]: Champion };
+    data:    T;
+}
+
+export interface Info {
+  attack:     number;
+  defense:    number;
+  magic:      number;
+  difficulty: number;
 }
 
 export interface Champion {
+  id:          string;
+  key:         string;
+  name:        string;
+  title:       string;
+  image:       Image;
+  skins:       Skin[];
+  lore:        string;
+  blurb:       string;
+  allytips:    string[];
+  enemytips:   string[];
+  tags:        string[];
+  partype:     string;
+  info:        Info;
+  stats:       { [key: string]: number };
+  spells:      Spell[];
+  passive:     Passive;
+  recommended: any[];
+}
+
+export interface Passive {
+  name:        string;
+  description: string;
+  image:       Image;
+}
+
+export interface Skin {
+  id:      string;
+  num:     number;
+  name:    string;
+  chromas: boolean;
+}
+
+export interface Datavalues {
+}
+
+export interface Leveltip {
+    label:  string[];
+    effect: string[];
+}
+
+export interface ChampionReduced {
     version: Version;
     id:      string;
     key:     string;
@@ -44,13 +92,6 @@ export enum Sprite {
     Champion5PNG = "champion5.png",
 }
 
-export interface Info {
-    attack:     number;
-    defense:    number;
-    magic:      number;
-    difficulty: number;
-}
-
 export enum Tag {
     Assassin = "Assassin",
     Fighter = "Fighter",
@@ -58,6 +99,29 @@ export enum Tag {
     Marksman = "Marksman",
     Support = "Support",
     Tank = "Tank",
+}
+
+export interface Spell {
+  id:           string;
+  name:         string;
+  description:  string;
+  tooltip:      string;
+  leveltip:     Leveltip;
+  maxrank:      number;
+  cooldown:     number[];
+  cooldownBurn: string;
+  cost:         number[];
+  costBurn:     string;
+  datavalues:   Datavalues;
+  effect:       Array<number[] | null>;
+  effectBurn:   Array<null | string>;
+  vars:         any[];
+  costType:     string;
+  maxammo:      string;
+  range:        number[];
+  rangeBurn:    string;
+  image:        Image;
+  resource:     string;
 }
 
 export enum Version {
@@ -70,9 +134,15 @@ function fetchRiot(path: string) {
     return fetch(`${baseURL}/${path}`)
 }
 
-export async function getChampions(): Promise<Data['data']> {
+export async function getChampions(): Promise<Data<{ [key: string]: ChampionReduced }>['data']> {
     const championsRes = await fetchRiot(`champion.json`)
     const { data } = (await championsRes.json());
 
     return data;
+}
+
+export async function getChampion(name: string): Promise<Data<Champion>['data']> {
+  const championsRes = await fetchRiot(`champion/${name}.json`)
+  const { data } = (await championsRes.json());
+  return data[name];
 }
