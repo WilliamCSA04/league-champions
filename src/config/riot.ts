7c7ccebb9a8f50cd99aefaf51070b2bf555248b4
williamcsa04 <williamcaetano14@gmail.com>
2023-01-20 22:31:02 -0300

    :sparkles: Added spell

--- src/config/riot.ts
+++ src/config/riot.ts
@@ -1,3 +1,4 @@
 export const BASE_DRAGON_URL = 'http://ddragon.leagueoflegends.com/cdn'
 export const SPLASH_PATH = '/img/champion/splash'
 export const LOADING_PATH = '/img/champion/loading'
+export const SPELL_PATH = '/img/spell'
--- src/utils/riot.ts
+++ src/utils/riot.ts
@@ -1,5 +1,9 @@
-import { BASE_DRAGON_URL, LOADING_PATH } from "src/config/riot";
+import { BASE_DRAGON_URL, LOADING_PATH, SPELL_PATH } from "src/config/riot";
 
 export function getLoadingURL(path: string) {
   return `${BASE_DRAGON_URL}${LOADING_PATH}/${path}_0.jpg`
+}
+
+export function getSpellURL(path: string) {
+  return `${BASE_DRAGON_URL}/13.1.1${SPELL_PATH}/${path}`
 }
